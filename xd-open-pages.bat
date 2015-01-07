6025470978f2ce4dacddf27f19c0604516f1dc0d
Kamran Ahmad <Aakhroot@users.noreply.github.com>
2015-01-07 14:52:52 -0500

    Added rabbit mq admin gui url

--- xd-open-pages.bat
+++ xd-open-pages.bat
@@ -12,6 +12,7 @@ timeout /T %CMD_TIMEOUT% /nobreak
 cd C:\Program Files (x86)\Google\Chrome\Application
 start /min %XD_INSTALL_DIR%/docs/index.html
 start /min %XD_ADMIN_GUI%
+start /min %RABBITMQ_ADMIN_GUI%
 
 exit
 
